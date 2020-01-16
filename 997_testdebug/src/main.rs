9d0e76d34095ff308164ce2a649cd2a3633adc44
Jean Fiedler <JFI@kd.local>
2020-01-16 13:38:02 +0100

    getting closer

--- 997_testdebug/src/main.rs
+++ 997_testdebug/src/main.rs
@@ -2,6 +2,14 @@ extern crate winreg;
 
 use winreg::enums::{HKEY_LOCAL_MACHINE, KEY_READ};
 
+fn bytes_to_hex (input: &winreg::RegValue) -> String
+{
+    let mut output: String = "".to_string();
+    for x in &input.bytes {
+        output = format!("{} {}", output, format!("{:02X}", x));
+    }
+    return output;
+}
 
 fn main() {
     let hklm = winreg::RegKey::predef(HKEY_LOCAL_MACHINE);
@@ -13,5 +21,7 @@ fn main() {
                     .expect("Failed to read InstallTime");
     
     println!("InstallTime : {:#?}", install_time.bytes);
+
+    println!("[{}]", bytes_to_hex(&install_time));
 
 }
