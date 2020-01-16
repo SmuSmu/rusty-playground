604eb216f02458ed58c5983fe2c28633db3dbfbd
Jean Fiedler <JFI@kd.local>
2020-01-16 12:57:55 +0100

    Testing Debug Output

--- testdebug/src/main.rs
+++ testdebug/src/main.rs
@@ -1,3 +1,17 @@
+extern crate winreg;
+
+use winreg::enums::{HKEY_LOCAL_MACHINE, KEY_READ};
+
+
 fn main() {
-    println!("Hello, world!");
+    let hklm = winreg::RegKey::predef(HKEY_LOCAL_MACHINE);
+    
+    // ProductName
+    let subkey = hklm.open_subkey_with_flags(r#"SOFTWARE\Microsoft\Windows Defender"#, KEY_READ)
+                    .expect("Failed to open subkey");
+    let install_time: winreg::RegValue = subkey.get_raw_value("InstallTime")
+                    .expect("Failed to read InstallTime");
+    
+    println!("InstallTime : {:#?}", install_time.bytes);
+
 }
